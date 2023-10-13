8d0a57964fa664bee76ddb0a502cc615b99b7409
Hetang Mehta <112903225+Killinwraith@users.noreply.github.com>
2023-10-12 21:13:22 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var r = document.querySelector('.siteHeader');
-
-let viewportHeight = window.innerHeight;
-let viewportWidth = window.innerWidth;
-
-console.log(viewportHeight, viewportWidth);
-
-r.style.setProperty('--width', 'viewportWidth');
